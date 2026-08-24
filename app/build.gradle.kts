0713a585731653d898be8b85376fdbc7fb3497a6
Peter <peterglennhill@gmail.com>
2026-08-24 12:48:55 -0600

    Updated versioning (11, 1.1.3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.peter.dailypsalms"
         minSdk = 26
         targetSdk = 37
-        versionCode = 10
-        versionName = "1.1.2"
+        versionCode = 11
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
