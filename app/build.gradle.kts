74467f9c7f44d1e959c98e5cc8616919b3ea570c
Peter <peterglennhill@gmail.com>
2026-09-07 11:27:16 -0600

    Fix: Enable R8 minification to resolve Play Console obfuscation warning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,10 +22,13 @@ android {
     }
 
     buildTypes {
-        release {
-            optimization {
-                enable = false
-            }
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
